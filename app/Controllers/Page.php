b174db066140b67528b2fef81c0fcacd0fed2ffb
Andika pratama <litageming@gmail.com>
2023-09-15 08:30:50 +0700

    up

---
+++ app/Controllers/Page.php
@@ -0,0 +1,23 @@
+<?php namespace App\Controllers;
+
+class Page extends BaseController
+{
+	public function about()
+	{
+		echo view("about");
+	}
+    
+    public function contact()
+	{
+		echo view("contact");
+	}
+    
+    public function faqs()
+	{
+		echo view("faqs");
+    }
+public function tos()
+    {
+        echo "Halaman Term of Service";
+    }
+}
